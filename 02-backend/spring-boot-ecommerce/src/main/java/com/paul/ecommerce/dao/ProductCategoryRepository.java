d2b61daa769e2fb47c058ae8845aa4bb5e76ee9e
sean609002 <pau22360801@gmail.com>
2024-07-02 22:50:34 +0800

    implementation finished: create a component for menu sidebar and makes routerLink and its content dynamic

--- 02-backend/spring-boot-ecommerce/src/main/java/com/paul/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/paul/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.paul.ecommerce.dao;
 import com.paul.ecommerce.Entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(value = "http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -2,25 +2,38 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
-  
+  private categoryUrl = 'http://localhost:8080/api/product-category';
   constructor(private httpClient : HttpClient) { }
   
   getProductList(categoryId : number) : Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByProductCategoryId?id=${categoryId}`;
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategoryList() : Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded : {
     products : Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded : {
+    productCategory : ProductCategory[];
   }
 }
