adcbd06394cf2465875930c1401271bf0c3a8638
sean609002 <pau22360801@gmail.com>
2024-07-28 16:04:41 +0800

    centralize cors config for all controllers in MyAppConfig

--- 02-backend/spring-boot-ecommerce/src/main/java/com/paul/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/paul/ecommerce/controller/CheckoutController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(value = "http://localhost:4200", allowCredentials="true")
+
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
--- 02-backend/spring-boot-ecommerce/src/main/java/com/paul/ecommerce/controller/UserController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/paul/ecommerce/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials="true")
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
